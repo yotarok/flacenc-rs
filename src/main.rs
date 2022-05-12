3ea122921db827108f09d6e6a07e87c723114442
Yotaro Kubo <yotaro@google.com>
2022-05-12 17:32:41 +0900

    Adds reporitory specification to Cargo.toml.
    and activates "clippy:cargo" clippy rules.

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![warn(
-    clippy::all,
-    clippy::nursery,
-    clippy::pedantic,
-// tentatively disabled
-//    clippy::cargo,
-)]
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, clippy::cargo)]
 // Some of clippy::pedantic rules are actually useful, so use it with a lot of
 // ad-hoc exceptions.
 #![allow(
