ead102a20128086f35567d31baa861c5ca26680c
Yotaro Kubo <yotaro@google.com>
2023-09-27 01:10:13 +0900

    Add single-thread config to report.

--- testtool/reporter.py
+++ testtool/reporter.py
@@ -35,6 +35,7 @@ REFERENCE_ENCODER_OPTS = {
 }
 TEST_ENCODER_OPTS = {
     "default": [],
+    "st": ["-c", "report/st.config.toml"],
     "dmse": ["-c", "report/dmse.config.toml"],
     "bsbs": ["-c", "report/bsbs.config.toml"],
     "mae": ["-c", "report/mae.config.toml"],
