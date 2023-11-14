3f60a0315e33691c132d18c1c5245d7fdac3e280
Yotaro Kubo <yotaro@google.com>
2023-11-14 16:57:05 +0900

    Rename `{MIN|MAX}_BLOCKSIZE` to `{MIN|MAX}_BLOCK_SIZE`.
    This is for consistency with other part of code.

--- fuzz/fuzz_targets/frame_encode.rs
+++ fuzz/fuzz_targets/frame_encode.rs
@@ -150,7 +150,7 @@ impl Input {
 impl<'a> Arbitrary<'a> for Input {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self, arbitrary::Error> {
         let channel_count = u.int_in_range(1usize..=constant::MAX_CHANNELS)?;
-        let block_size = u.int_in_range(constant::MIN_BLOCKSIZE..=constant::MAX_BLOCKSIZE)?;
+        let block_size = u.int_in_range(constant::MIN_BLOCK_SIZE..=constant::MAX_BLOCK_SIZE)?;
         let sample_rate = u.int_in_range(1usize..=192_000)?;
         let bits_per_sample = (u.int_in_range(2u8..=6u8)? * 4) as usize;
 
--- src/component.rs
+++ src/component.rs
@@ -30,10 +30,10 @@ use super::constant::qlpc::MAX_PRECISION as MAX_LPC_PRECISION;
 use super::constant::qlpc::MAX_SHIFT as MAX_LPC_SHIFT;
 use super::constant::qlpc::MIN_SHIFT as MIN_LPC_SHIFT;
 use super::constant::MAX_BITS_PER_SAMPLE;
-use super::constant::MAX_BLOCKSIZE;
+use super::constant::MAX_BLOCK_SIZE;
 use super::constant::MAX_CHANNELS;
 use super::constant::MIN_BITS_PER_SAMPLE;
-use super::constant::MIN_BLOCKSIZE;
+use super::constant::MIN_BLOCK_SIZE;
 use super::error::verify_range;
 use super::error::verify_true;
 use super::error::OutputError;
@@ -179,7 +179,7 @@ const fn block_size_spec(block_size: u16) -> (u8, u16, usize) {
 // Some (internal) utility macros for value verification.
 macro_rules! verify_block_size {
     ($varname:literal, $size:expr) => {
-        verify_range!($varname, $size, MIN_BLOCKSIZE..=MAX_BLOCKSIZE)
+        verify_range!($varname, $size, MIN_BLOCK_SIZE..=MAX_BLOCK_SIZE)
     };
 }
 macro_rules! verify_bps {
